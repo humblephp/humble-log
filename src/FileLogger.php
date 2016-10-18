840b74742057d92afd0e98e2af4203941772fbe3
Arek Wiertlewski <arek@jobs.ac.uk>
2016-10-18 17:22:14 +0100

    log to file

--- src/FileLogger.php
+++ src/FileLogger.php
@@ -7,6 +7,13 @@ use Psr\Log\LoggerInterface;
 
 class FileLogger extends AbstractLogger implements LoggerInterface
 {
+    private $path;
+
+    public function __construct($path)
+    {
+        $this->path = $path;
+    }
+
     /**
      * Logs with an arbitrary level.
      *
@@ -18,5 +25,16 @@ class FileLogger extends AbstractLogger implements LoggerInterface
      */
     public function log($level, $message, array $context = array())
     {
+        $dir = $this->path . '/' . date('Y') . '/' . date('m');
+
+        if (!file_exists($dir)) {
+            mkdir($dir, 0777, true);
+        }
+
+        file_put_contents(
+            sprintf('%s/%s.txt', $dir, date('d')),
+            sprintf('%s [%s] %s %s', date('H:i:s'), $level, $message, json_encode($context)) . PHP_EOL,
+            FILE_APPEND
+        );
     }
 }
